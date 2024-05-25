8ba984f9041495e1c009d6f9d3a921a778a368fe
Kunal Lingwal <kunallingwal2@gmail.com>
2024-05-25 23:27:08 +0530

    fix:updatedd component

--- components/TracingBeamDemo.js
+++ components/TracingBeamDemo.js
@@ -79,7 +79,7 @@ Power Management: Efficient battery usage and low-power components to extend ope
             "https://images.unsplash.com/photo-1519681393784-d120267933ba?auto=format&fit=crop&q=80&w=3540&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-        title: "Lorem Ipsum Dolor Sit Amet",
+        title: "",
         description: (
             <>
             <p>
